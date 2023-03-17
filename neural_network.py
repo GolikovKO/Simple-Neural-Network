c1d0ed9139ef14e86506bfdc4eff235fac5f8742
steben_op <kostya.battlefield13@mail.ru>
2023-03-17 09:26:33 +0400

    [upd] -- add query and train functions

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,43 @@
+import numpy
+import scipy
+
+
 class NeuralNetwork:
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         self.i_nodes = input_nodes
         self.h_nodes = hidden_nodes
         self.o_nodes = output_nodes
 
+        self.wih = numpy.random.normal(0.0, pow(self.h_nodes, -0.5), (self.h_nodes, self.i_nodes))
+        self.who = numpy.random.normal(0.0, pow(self.o_nodes, -0.5), (self.o_nodes, self.h_nodes))
+
         self.l_rate = learning_rate
 
-    def train(self):
-        pass
+        self.activation_function = lambda x: scipy.special.expit(x)
 
-    def query(self):
-        pass
+    def train(self, inputs_list, targets_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
 
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.l_rate * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                            numpy.transpose(hidden_outputs))
+        self.wih += self.l_rate * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                            numpy.transpose(inputs))
+
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        return final_outputs
